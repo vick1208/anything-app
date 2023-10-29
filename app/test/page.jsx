834a5ce4cbbe1fa4dca4fe5ecbabfddaf8ecf5d5
Vicky AS <vickyhuang808@gmail.com>
2023-10-29 17:02:38 +0700

    trying state

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,11 +1,20 @@
 "use client"
 import Button from '@/app/test/component/button';
+import { useState } from 'react';
 export default function Test() {
 
-    return (<div>
-        <h1>Hello World!</h1>
-        <Button />
-        <h3>0</h3>
+    const [counts, setCounts] = useState(0);
+    function handleClicks() {
+        setCounts(counts + 1);
+    }
+
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <Button />
+            <br />
+            <h3>{counts}</h3>
+            <button onClick={handleClicks}>Add the number</button>
         </div>);
 
 }
